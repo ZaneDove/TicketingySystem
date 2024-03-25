64e303a18349279b24a99091372178958fabee2b
ZaneDove <74105889+ZaneDove@users.noreply.github.com>
2024-03-25 12:42:12 +0000

    Java for workers

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,6 +12,8 @@ import io.camunda.zeebe.client.api.worker.JobClient;
 import io.camunda.zeebe.spring.client.EnableZeebeClient;
 import io.camunda.zeebe.spring.client.annotation.ZeebeWorker;
 
+import static com.fasterxml.jackson.core.io.NumberInput.parseAsInt;
+
 @SpringBootApplication
 @EnableZeebeClient
 public class Main {
@@ -22,20 +24,61 @@ ArrayList<Ticket> ticketArrayList = new ArrayList<Ticket>();
     //open ticket worker
     @ZeebeWorker(type = "OpenTicket")
     public void OpenTicket(final JobClient client, final ActivatedJob job){
+        //get variables as map
         Map<String, Object> variablesAsMap = job.getVariablesAsMap();
+        // get priority and issueDescription
         String priority = (String) variablesAsMap.get("priority");
         String info = (String) variablesAsMap.get("issueDescription");
+        //new ticket
         Ticket ticket;
+        //if list is empty tiket no = 1, else tikcetno = list size + 1
         if (ticketArrayList.isEmpty()){
             ticket = new Ticket(1, info, true, priority);
         }else {
             ticket = new Ticket((ticketArrayList.size() + 1), info, true, priority);
         }
+        //add to list
         ticketArrayList.add(ticket);
+        //add tikcet no to map
         HashMap<String, Object> variables = new HashMap<>();
+        //complete job send ticketNo
         variables.put("ticketNo",ticket.getTicketNo());
         client.newCompleteCommand(job.getKey())
                 .variables(variables)
+                .send()
+                .exceptionally((throwable -> {
+                    throw new RuntimeException("Could not complete job", throwable);
+                }));
+
+    }
+    @ZeebeWorker(type = "DetermineSLA")
+    public void DetermineSLA(final JobClient client, final ActivatedJob job){
+        // job complete
+        client.newCompleteCommand(job.getKey())
+                .send()
+                .exceptionally((throwable -> {
+                    throw new RuntimeException("Could not complete job", throwable);
+                }));
+
+    }
+    @ZeebeWorker(type = "closeTicket")
+    public void closeTicket(final JobClient client, final ActivatedJob job){
+        // get variables as map
+        Map<String, Object> variablesAsMap = job.getVariablesAsMap();
+        // get ticket no as string
+        String ticketNoAsString = (String) variablesAsMap.get("ticketNo");
+        //Convert to int
+        int ticketNo = Integer.parseInt(ticketNoAsString);
+        //get ticket from array list (ticketNo - 1)
+        Ticket tempTicket = ticketArrayList.get(ticketNo-1);
+        // ticket open = false
+        tempTicket.setTicketOpen(false);
+        //ticket in arrray = temp ticket
+        ticketArrayList.set(ticketNo - 1, tempTicket);
+        //print ticket closed
+        System.out.print("ticket " + ticketNo + " closed");
+        //job complete
+        client.newCompleteCommand(job.getKey())
                 .send()
                 .exceptionally((throwable -> {
                     throw new RuntimeException("Could not complete job", throwable);
